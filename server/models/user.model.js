5beb5d80687a17359a67f5920694ce496f24fc41
Stephen Kalwenzi <kalwenzistephen2003@gmail.com>
2025-07-10 08:33:48 +0300

    add hashing

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,37 +1,61 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userschema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    unique: false,
-    trim: true,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      unique: false,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      unique: false,
+      trim: true,
+    },
+    email: {
+      type: string,
+      unique: true,
+      trim: true,
+      required: true,
+      match: [/.+@.+\..+/, "Please fill a valid email address"], // Basic email validation
+    },
+    userName: {
+      type: string,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    password: {
+      required: true,
+      type: string,
+      minlength: 6,
+    },
   },
-  lastName: {
-    type: String,
-    unique: false,
-    trim: true,
-  },
-  email: {
-    type: string,
-    unique: true,
-    trim: true,
-    required: true,
-    match: [/.+@.+\..+/, "Please fill a valid email address"], // Basic email validation
-  },
-  userName: {
-    type: string,
-    unique: true,
-    trim: true,
-    required: true,
-  },
-  password: {
-    required: true,
-    type: string,
-    minlength: 6,
-  }},{
-    timestamps:true
+  {
+    timestamps: true,
   }
 );
 
+UserSchema.pre("save", async function (next) {
+  //check if password has been modified
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-modules.export = userschema
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// --- Method to compare password for login ---
+userschema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword,this.password)
+}
+
+module.exports = mongoose.model("User", UserSchema);
