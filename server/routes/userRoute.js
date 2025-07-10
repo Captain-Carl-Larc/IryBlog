e58a2769fcc9ae3ca29402555ae53331a2d469dc
Stephen Kalwenzi <kalwenzistephen2003@gmail.com>
2025-07-10 10:40:16 +0300

    user routes made

---
+++ server/routes/userRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const {
+  registerUser,
+  loginUser,
+  getAllUsers,
+} = require("../controllers/user.controller");
+
+const router = express.Router()
+
+router.post('/register',registerUser)
+
+router.post('/login',loginUser)
+
+router.get("/", getAllUsers);
+
+module.exports = router
