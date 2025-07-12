ba63f0e32d863cfebdc16ba84a1fe9ce4d0d3230
Stephen Kalwenzi <kalwenzistephen2003@gmail.com>
2025-07-12 19:08:07 +0300

    get all posts route

--- server/routes/blogRoute.js
+++ server/routes/blogRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {createBlog} = require('../controllers/blog.controller')
+const { createBlog, getAllPosts } = require("../controllers/blog.controller");
 const {protect} = require('../middleware/authmiddleware')
 
 const router = express.Router()
@@ -7,5 +7,8 @@ const router = express.Router()
 
 //route to create a post
 router.post("/create",protect, createBlog);
+
+//route to get all posts
+router.get("/",protect, getAllPosts);
 
 module.exports = router
