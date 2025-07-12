9bc45aca0a17c8202cbca21bb251a4bba0bcc565
Stephen Kalwenzi <kalwenzistephen2003@gmail.com>
2025-07-12 19:42:04 +0300

    add route to get posts of specifi user

--- server/routes/blogRoute.js
+++ server/routes/blogRoute.js
@@ -3,6 +3,7 @@ const {
   createBlog,
   getAllPosts,
   getSinglePost,
+  getPostsOfUser,
 } = require("../controllers/blog.controller");
 const {protect} = require('../middleware/authmiddleware')
 
@@ -16,6 +17,9 @@ router.post("/create",protect, createBlog);
 router.get("/",protect, getAllPosts);
 
 //route to get post by id
-router.get("/:id", protect, getSinglePost);
+router.get("/:postId", protect, getSinglePost);
+
+//get post by user id
+router.get("/author/:authorId", protect, getPostsOfUser);
 
 module.exports = router
