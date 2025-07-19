6737f6f9e8d17a1e74ff3d747252e74bcb6f61ea
Stephen Kalwenzi <kalwenzistephen2003@gmail.com>
2025-07-19 09:58:20 +0300

    created about page

---
+++ client/src/pages/about.jsx
@@ -0,0 +1,11 @@
+function about() {
+    return ( 
+        <>
+            <h1 className="font-black">
+                about page here
+            </h1>
+        </>
+     );
+}
+
+export default about;
