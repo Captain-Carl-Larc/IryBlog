d389d9784c7956c3f48499edb50ca5614af84985
Stephen Kalwenzi <kalwenzistephen2003@gmail.com>
2025-07-19 09:58:36 +0300

    created blog details page

---
+++ client/src/pages/blog.jsx
@@ -0,0 +1,12 @@
+function blogItem() {
+    return ( <>
+        <h1 className="font-black text-blue-600 text-4xl">
+            Individual blog page
+        </h1>
+        <p>
+            page to read blog details
+        </p>
+    </> );
+}
+
+export default blogItem;
